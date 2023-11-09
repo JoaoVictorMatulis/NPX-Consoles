cba8cb92093803322730d61096f58cdbd2a25626
João Victor Matulis <110988253+JoaoVictorMatulis@users.noreply.github.com>
2023-11-09 09:44:49 -0300

    Update index.js

--- npx-react/src/pages/home/index.js
+++ npx-react/src/pages/home/index.js
@@ -129,7 +129,7 @@ export default function Contato() {
                         <div class="desenvolvido">
                             <h1>DESENVOLVIDO POR:</h1>
                             <p>BERNARDO GALVÃO DE SOUZA<br></br>
-                                JOÃO VICTOR MATULIS<br>
+                                JOÃO VICTOR MATULIS<br></br>
                                 KEVIN SOUSA DOS SANTOS</p>
                         </div>
                         <div clas="rodape-img">
